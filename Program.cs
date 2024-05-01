fa69384fc6c06f8477ddecfbcfb72c24851835bf
David Melo <124303430+davidmelo84@users.noreply.github.com>
2024-05-01 12:17:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿using DesafioPOO.Models;
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma instância de Nokia
+            Nokia nokia = new Nokia("10102020", "Nokia Xurubim", "2021202223", 88);
+            nokia.Ligar(); // Método da classe base
+            nokia.ReceberLigacao(); // Método da classe base
+            nokia.InstalarAplicativo("TikTok"); // Método da classe Nokia
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+            // Criando uma instância de iPhone
+            iPhone iphone = new iPhone("587421897", "iPhone 80", "1441255263", 321);
+            iphone.Ligar(); // Método da classe base
+            iphone.ReceberLigacao(); // Método da classe base
+            iphone.InstalarAplicativo("Instagram"); // Método da classe iPhone
+        }
+    }
+}
